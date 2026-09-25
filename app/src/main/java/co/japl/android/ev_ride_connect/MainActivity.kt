043d33661bc43a7d41d9e0ce8cce77ed43dd23c3
Jose Alejandro Parra Lasso <alejandro.parralasso@xpo.com>
2026-09-25 17:55:00 -0500

    actualizando interface

--- app/src/main/java/co/japl/android/ev_ride_connect/MainActivity.kt
+++ app/src/main/java/co/japl/android/ev_ride_connect/MainActivity.kt
@@ -7,16 +7,14 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import co.com.japl.homeconnect.about.ui.About
 import co.com.japl.ui.theme.MaterialThemeComposeUI
 import co.japl.android.ev_ride_connect.controller.BackupViewModel
 import co.japl.android.ev_ride_connect.controller.DashboardViewModel
@@ -166,10 +164,9 @@ fun AppNavigationContent(
             )
 
             AppScreen.ABOUT -> Box(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    text = stringResource(R.string.about),
-                    style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.onSurface
+                About(
+                    versionDetail = version,
+                    applicationId = appId
                 )
             }
 
